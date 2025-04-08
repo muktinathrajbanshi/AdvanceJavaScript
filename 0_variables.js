4b0bacff0d9b02a002f749864a2b37c7e4a5e6ed
unknown <muktinathrajbanshi786@gmail.com>
2025-04-08 21:37:25 +0545

    JavaScript variables and Datatypes are completed!

---
+++ 0_variables.js
@@ -0,0 +1,32 @@
+//* ====================================
+//* Values and Variable in JavaScript
+//* ====================================
+
+//* In JavaScript, values and variables are fundamental concepts that form the basis of programming.
+
+//* Values:  A value is a piece of information that a program can work with. It can be a number, text, true/false, or more complex data.
+
+//* Variables: A variable is a container that holds a value. It has a name and can be used to store and manipulate data in a program.
+
+
+//console.log("Welcome to my Country");
+
+var myAge = 24;
+console.log(myAge);
+
+
+//! Let's test
+    var my_firstName = "John";
+    console.log(my_firstName);
+
+    var _myLastName$ = "Doe";
+    console.log(_myLastName$);
+
+    //var 123myAge = 25;
+    //console.log(123myAge);
+
+   var $cityName = "New York";
+   console.log($cityName);
+
+   var my@Email = "john@example.com";
+   console.log(my@Email);
