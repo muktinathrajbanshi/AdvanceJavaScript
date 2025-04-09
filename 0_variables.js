e75c19b4116b4726d33796ff3280b88abd36829e
unknown <muktinathrajbanshi786@gmail.com>
2025-04-10 02:00:06 +0545

    JS expressions is completed

--- 0_variables.js
+++ 0_variables.js
@@ -18,15 +18,21 @@ console.log(myAge);
 //! Let's test
     var my_firstName = "John";
     console.log(my_firstName);
+    //? Explanation: This is a valid variable name. It starts with a letter, and the subsequent characters include letters, numbers, and an underscore.
+    //  Follows JavaScript naming rules.
+
 
     var _myLastName$ = "Doe";
-    console.log(_myLastName$);
+    //? Explanation: This is a valid variable name. It starts with an underscore, and the subsequent characters include letters, numbers, and a dolar sign. Follows JavaScript naming rules.
+
 
     //var 123myAge = 25;
-    //console.log(123myAge);
+    //? Explanation: This is not a valid variable name. It starts with a number, which is not allowed as per JavaScript naming rules. Variable names cannot begin with a digit.
 
    var $cityName = "New York";
-   console.log($cityName);
+   //? Explanation: This is a valid variable name. It starts with a dollar sign, and the subsequent characters include letters. Follows JavaScript naming rules.
 
-   var my@Email = "john@example.com";
-   console.log(my@Email);
+
+   //var my@Email = "john@example.com";
+   //? Explanation: This is not a valid variable name. It includes the special character '@', which is not allowed in JavaScript variable names. Only letters, numbers, underscores, and dollar signs are allowed.
+
--- 3_expressions.js
+++ 3_expressions.js
@@ -17,46 +17,55 @@
 //* 1: Assignment operators
 //* ====================================
 
-//? Assignment operators in programming are symbols used to assign values to variables. They take the value on the right side of the operator and assign it to the variable on the left side.
+//? Assignment operators in programming are symbols used to assign values to variables. They take the value on the right side of the operator
+//?  and assign it to the variable on the left side.
 
 // examples
+// var myFavNum = 15;
+// Assigns the value 15 to the variable myFavNum
+// var channelName = 'muktinath rajbanshi'
 
 //* ===================================
 //* 2: Arithmetic operators
 //* ====================================
 
-//? Arithmetic operators in programming perform basic mathematical operations on variables or values. They include addition, subtraction, multiplication, division, and modulus.
+//? Arithmetic operators in programming perform basic mathematical operations on variables or values. They include addition, subtraction, 
+//? multiplication, division, and modulus.
 
 //? Addition (+): Adds two values or variables.
-// Example:
-// let x = 5;
-// let y = 10;
-// let sum = x + y;
-// console.log(sum);
+    //Example:
+    // var x = 5;
+    // var y = 10;
+    // var sum = x + y
+    // console.log(sum);
 
 //? Subtraction (-): Subtracts the right operand from the left operand.
 // Example:
-// let a = 10;
-// let b = 7;
-// let difference = a - b;
+    // var a = 10;
+    // var b = 7;
+    // var difference = a - b;
+    // console.log(difference);
 
 //? Multiplication (*): Multiplies two values or variables.
 // Example:
-// let p = 4;
-// let q = 6;
-// let product = p * q;
+// var p = 4;
+// var q = 6;
+// var product = p * q;
+// console.log(product);
 
 //? Division (/): Divides the left operand by the right operand.
 // Example:
-// let m = 15;
-// let n = 3;
-// let quotient = m / n;
+    // var m = 15;
+    // var n = 3;
+    // var quotient = m / n;
+    // console.log(quotient);
 
 //? Modulus (%): Returns the remainder when the left operand is divided by the right operand.
 // Example:
-// let c = 17;
-// let d = 5;
-// let remainder = c % d;
+    // var c = 17;
+    // var d = 5;
+    // var remainder = c % d;
+    // console.log(remainder);
 
 //* ===================================
 //* Challenge Time
@@ -64,29 +73,44 @@
 
 //! What will be the Output 🤔💭
 // let result = "hello" / 2 ❓
+// var result = "hello" / 2;
+// console.log(result);
 
 //* ===================================
 //*  InterView Question
 //* ====================================
 
-//! let result = 0.1 + 0.2 ❓ 🤔💭
+//! var result = 0.1 + 0.2 ❓ 🤔💭
+// var result = 0.1 + 0.2;
+// console.log(result.toFixed(2));
+// when working with floating-point numbers in JavaScript, consider using methods like toFixed() when precise
+//decimal representation is necessary.
+
+// const result = 55 * "hello"  ?
+    // var result = 55 * "hello";
+    // console.log(result);
+    
+
+
 
 //* ===================================
 //* 3: String Operators
 //* ====================================
 
-//? There are a few ways to concatenate strings in JavaScript. The most common way is to use the + operator. For example, to concatenate the strings "Hello" and "World", you would use the following code:
+//? There are a few ways to concatenate strings in JavaScript. The most common way is to use the + operator. 
+//? For example, to concatenate the strings "Hello" and "World", you would use the following code:
 
-// var str1 = "Hello";
-// var str2 = "World";
-// var str3 = str1 + str2;
-// console.log(str3);
+    // var str1 = "Hello";
+    // var str2 = "World";
+    // var str3 = str1 + str2;
+    // console.log(str3);
 
 //* ===================================
 //*  InterView Question
 //* ====================================
 
 //! console.log("5" + 3);  // Outputs "53" ❓
+// console.log("5" + 3);
 
 //* ===================================
 //* 4: comparison operators
@@ -99,16 +123,16 @@
 
 //? Strict Equal (===):
 // Checks if two values are equal without performing type coercion.
-// console.log(5 === "5");
+    // console.log(5 === "5");
 
 //? Not Equal (!=   👉 ! =):
 // Checks if two values are not equal, performing type coercion if necessary.
-// console.log(5 != "5");
+    // console.log(5 != 5);
 
 //? Greater Than (>):
 // Checks if the value on the left is greater than the value on the right.
 // Example: 10 > 5 evaluates to true.
-// console.log(5 > 2);
+    // console.log(5 > 2);
 
 //? Less Than (<):
 // Checks if the value on the left is less than the value on the right.
@@ -118,7 +142,7 @@
 //? Greater Than or Equal To (>=):
 // Checks if the value on the left is greater than or equal to the value on the right.
 // Example: 10 >= 10 evaluates to true.
-// console.log(10 <= 10);
+    // console.log(10 <= 10);
 
 //? Less Than or Equal To (<=):
 // Checks if the value on the left is less than or equal to the value on the right.
@@ -130,6 +154,18 @@
 //* ====================================
 
 //! What is the difference between == and === operators in JavaScript❓
+//? The equality == operator is a comparison operator that compares two values and returns true if they are equal. 
+//? The strict equality === operator is also a comparison operator, but it compares two values true only if they
+//? are equal and of the same type.
+//example:
+// let num1 = 1;
+// let num2 = "1";
+
+// if (num1 === num2) {
+//     console.log("equal");
+// } else {
+//     console.log("not equal");
+// }
 
 //* ===================================
 //* 5: Logical operators in JavaScript
@@ -141,7 +177,7 @@
 // Example:
 // var x = 5;
 // var y = 10;
-// console.log(x > 0 && y > 0);
+// console.log(x > 0 && y < 0);
 
 //? Logical OR (||): Returns true if at least one of the operands is true, otherwise, it returns false.
 // Example:
@@ -160,12 +196,22 @@
 //* ====================================
 
 //? Combining logical operators allows you to create complex conditions:
-//! Q: Write a program that determines if a person is eligible to drive based on their age being greater than or equal to 18 and having a valid driver's license❓
+//! Q: Write a program that determines if a person is eligible to drive based on their age being greater than 
+//! or equal to 18 and having a valid driver's license❓
 
-// var age = 18;
+// var age = 19;
 // var hadDrivingLicense = true;
 
+// console.log(age >=18 && hadDrivingLicense);
+
+
+
 //! How would the result change if hasDriverLicense was set to false❓
+
+//   var age = 19;
+//   var hadDrivingLicense = false;
+
+//   console.log(age>=18 && hadDrivingLicense);
 
 //* ===================================
 //* 6: Conditional (ternary) operator
@@ -174,13 +220,24 @@
 //? syntax: condition ? expressionIfTrue : expressionIfFalse;
 // ! write a program to check if the candidates isEligibleToDrive or not? Age must be equal to or greater then 18.
 
-//! Q: Let's say you have a variable score representing a student's exam score. If the score is greater than or equal to 60, the student passes; otherwise, they fail. Use the conditional (ternary) operator to determine the result and store it in a variable called result. Log the result to the console❓
+    // var age = 19;
+    // var result = age>=18? "Yes" : "No";
+    // console.log(result);
+
+//! Q: Let's say you have a variable score representing a student's exam score. If the score is greater
+//!  than or equal to 60, the student passes; otherwise, they fail. Use the conditional (ternary) operator 
+//! to determine the result and store it in a variable called result. Log the result to the console❓
+
+//  var score = 90;
+//  var result = score>=60?"Pass":"Fail";
+//  console.log(result);
 
 //* ===================================
 //*  Combined Interview Questions
 //* ====================================
 
 // console.log("5" - 3);
+// console.log(typeof ("5" - 3));
 
 // console.log(2 < 12 < 5);
 
