dfdfeb374e09f91af524373f6e833968e48476e2
unknown <muktinathrajbanshi786@gmail.com>
2025-04-25 22:19:59 +0545

    ecmascripts 2016 completed

--- ecmascripts/es2016.js
+++ ecmascripts/es2016.js
@@ -6,7 +6,9 @@
 //*    Exponentiation Operator
 //* ===============================
 
-//? ES7 introduces a new mathematical operator called exponentiation operator. This operator is similar to using Math.pow() method. Exponentiation operator is represented by a double asterisk **. The operator can be used only with numeric values.
+//? ES7 introduces a new mathematical operator called exponentiation operator. This operator is similar to using 
+//? Math.pow() method. Exponentiation operator is represented by a double asterisk **. The operator can be used 
+//? only with numeric values.
 
 //* syntax
 // base_value ** exponent_value;
@@ -14,11 +16,13 @@
 //? Basic usage:
 let base = 2;
 let exponent = 3;
+// console.log("using Math.pow()", Math.pow(base, exponent));
+// console.log("using exponential operator", base ** exponent);
 
 //? In expressions:
 //! calculates the area of a circle with a radius of 5 units.
-// let area = Math.PI * 5 ** 2;
-// console.log(area.toFixed(2));
+    let area = Math.PI * 5 ** 2;
+    console.log(area.toFixed(2));
 
 //* ==============================
 //*  Array.includes()
