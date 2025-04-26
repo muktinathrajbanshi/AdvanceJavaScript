491d4bfc4b4421b5c260f544e595c3a5acdae908
unknown <muktinathrajbanshi786@gmail.com>
2025-04-26 23:55:10 +0545

    ecmascript 2023 completed

--- ecmascripts/es2023.js
+++ ecmascripts/es2023.js
@@ -14,19 +14,36 @@
 //*  Array.findLast() & Array.findLastIndex()
 //* ==============================================
 //? This function will allow us to find element from the last to first of the array based on a condition.
-// const array = [1, 2, 3, 4, 5, 6, 4];
+    // const array = [1, 2, 3, 4, 5, 6, 4];
+    // console.log(array.findLast((curElem) => curElem > 4));
+    // console.log(array.findLastIndex((curElem) => curElem));
 
 //* ===============================================
 //*  New Array.prototype functions
 //* ==============================================
 
-const myNames = ["vinod", "bahadur", "thapa", "kodyfier"];
+const myNames = ["muktinath", "rajbanshi", "ok", "kodyfier"];
 
 //? Array.prototype.toReversed();
+    // const reversedNum = myNames.toReversed();
+    // console.log("original", myNames);
+    // console.log("reversed", reversedNum);
 
 //? Array.prototype.toSorted(compareFn);
+    // const sortedArr = myNames.toSorted();
+    // console.log("original", myNames);
+    // console.log("sorted", sortedArr);
 
 //? Array.prototype.toSpliced(start, deleteCount, ...items);
+    // const splicedArr = myNames.toSpliced(1, 1, "Rose");
+    // console.log("original", myNames);
+    // console.log("spliced", splicedArr);
 
 //? Array.prototype.with(index, value);
-//* The with() method in JavaScript is used to change the value of an element at a specific index in an array. It takes two arguments: the index of the element to be changed and the new value. It returns a new array with the changed element, leaving the original array unchanged.
+//* The with() method in JavaScript is used to change the value of an element at a specific index in an array. 
+//* It takes two arguments: the index of the element to be changed and the new value. It returns a new array with
+//*  the changed element, leaving the original array unchanged.
+
+    const replaceWith = myNames.with(1, "@muktinathrajbanshi219");
+    console.log("original", myNames);
+    console.log("replaced", replaceWith);
